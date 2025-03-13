40f96c6eb70c42bafa98f2f052ab389328dea661
Mahred1 <mredwan.kemal@gmail.com>
2025-03-13 09:56:39 +0300

    added the header component with an input and button and basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <div>
-    
+  return <div className="app">
+    <Header />
   </div>;
 }
 
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="header">
+           <input className="input" placeholder="Add a new task"></input> 
+           <button>+</button>
+        </header>
+    )
+}
+
+export default Header
