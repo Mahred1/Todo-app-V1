8926f3dc42b7668d6b0e6fd9328d7b4ac473f6fa
Mahred1 <mredwan.kemal@gmail.com>
2025-03-14 10:22:06 +0300

    enabled selecting the input field when Enter is pressed, and prevented the ability to create todos without a title

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,35 @@
-function Header({ input, onSetInput,todos,onSetTodos }) {
-  function handleAddTodo() {
+import { useEffect, useRef } from "react";
 
-    onSetTodos(()=>[...todos,newTodo])
+function Header({ input, onSetInput, todos, onSetTodos }) {
+  const task = useRef(null);
+
+  useEffect(function () {
+    function callback(e) {
+      if (document.activeElement === task.current) return;
+
+      if (e.key === "Enter") {
+        task.current.focus();
+      }
+    }
+    document.addEventListener("keypress", (e) => callback(e));
+  }, []);
+  function handleAddTodo() {
+    if (input !== "") onSetTodos(() => [...todos, newTodo]);
     const newTodo = {
       title: input,
       id: Math.floor(Math.random() * 100000),
       done: false,
     };
-   onSetInput("")
+    onSetInput("");
   }
   return (
     <header className="header">
       <input
-      value={input}
+        value={input}
         onChange={(e) => onSetInput(e.target.value)}
         className="input"
         placeholder="Add a new task"
+        ref={task}
       ></input>
       <button onClick={() => handleAddTodo()}>+</button>
     </header>
